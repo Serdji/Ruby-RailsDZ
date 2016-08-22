9cc5a800de54010303113c9279166620e259c9c2
s.sumatokhin <s.sumatokhin@rambler-co.ru>
2016-08-22 17:59:07 +0300

    les-2 fix one

--- lesson-2/index.rb
+++ lesson-2/index.rb
@@ -1,1 +1,34 @@
-test
+
+2
+
+player = { name: 'johnny', color: :red }
+colors = [:blue, :white, :green, :red, :orange]
+
+p player[:color] == colors.sample
+
+3
+
+names = %w[ambientsketchbook Erik\ Wollo Brian\ Eno Evangelos\ Papathanassiou Shulman]
+
+p names.select{|el| el.size < 10 && el[0] == el[0].upcase}
+
+4
+
+arr = ["Ruby", "Python", "JavaScript", "Java", ".NET", "HTML", "Clojure"]
+
+hash = arr.inject({}) do |memo, values|
+  memo.merge!(values => values.size)
+end
+
+hash["Go"] = 2
+
+p hash
+
+5
+
+def fibonacci(n)
+    return 1 if n < 3
+    fibonacci(n - 1) + fibonacci(n - 2)
+end    
+
+p fibonacci (10)	    

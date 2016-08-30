ffe7c4a98a67a1e272009e39e16f15c4a8426033
s.sumatokhin <s.sumatokhin@rambler-co.ru>
2016-08-30 17:17:11 +0300

    las-3 fix two

--- lesson-3/index.rb
+++ lesson-3/index.rb
@@ -1,5 +1,11 @@
 elements = [:one, :two, :three, :four]
 
-def combination arr, number
-  
+def combinations(array, number)
+  m = array.size
+  arr = (1...2**m).map do | n |
+    (0...m).select { | i | n[i] == 1 }.map { | i | array[i] }
+  end
+  arr.select{|x| x.size == number}
 end
+
+p combinations(elements, 3)
